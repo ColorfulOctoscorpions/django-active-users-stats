9310136b2e09f02521934c64ef5c6d2668b85205
Jan Warchoł <jan.warchol@gmail.com>
2020-12-11 18:24:17 +0100

    Only track specific couchdb requests

--- active_users/middleware.py
+++ active_users/middleware.py
@@ -9,4 +9,5 @@ class ActiveUsersMiddleware(MiddlewareMixin):
     """Tracks activity of users (count of actions per day)."""
     def process_request(self, request):
         if request.user.is_authenticated and not is_blacklisted(request):
-            Activity.objects.increment_now(user=request.user)
+            if request.path == '/db/_bulk_docs':
+                Activity.objects.increment_now(user=request.user)
